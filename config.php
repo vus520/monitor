fda9e309e4f61bfae313018bc689d583c307bd4a
尘缘 <admin@4wei.cn>
2016-01-28 15:23:17 +0800

    代理一个小时一过期，保证可用性

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ define("MAX_CHECK_NUM", 10);
 
 //代理库文件，默认按天获取最新的代理ip
 //如果是固定代理ip，则固定该文件名，一行一个
-$proxy = DIR."cache/proxy/".date("Ymd");
+$proxy = DIR."cache/proxy/".date("YmdH");
 $proxy_tmp = DIR."cache/proxy/tmp";
 
 if(!is_dir(dirname($proxy)))
